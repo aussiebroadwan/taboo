3321b47912a63c758d66346153912d1f27c85f27
Lachlan Cox <lcox74@outlook.com>
2025-03-24 21:39:41 +1100

    feat: allow both discord and web access

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,28 +2,27 @@ import { Engine } from "./engine";
 import { LiveDrawScene } from "./scenes/LiveDrawScene";
 
 import Client from './network/Client.js';
-
 import { DESIGN_WIDTH, DESIGN_HEIGHT } from './constants';
 
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
-// Instantiate the SDK
-const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+let usingDiscordSDK = false;
 
-setupDiscordSdk().then(() => {
-  console.log("Discord SDK is ready");
-});
+// Check if the URL has the "frame_id" query parameter.
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has("frame_id")) {
+    usingDiscordSDK = true;
 
-async function setupDiscordSdk() {
-  await discordSdk.ready();
+    // Instantiate and set up the Discord SDK.
+    const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+    discordSdk.ready().then(() => console.log("Discord SDK is ready"));
 }
-
 
 const setupWebsocket = (scene) => {
     // Build the WebSocket URL based on the frontend's location.
     const protocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
     const hostname = window.location.host;
-    const wsUrl = `${protocol}${hostname}/.proxy/ws`;
+    const wsUrl = `${protocol}${hostname}${usingDiscordSDK ? '/.proxy' : ''}/ws`;
 
     // Prepare Connection
     const wsClient = new Client(wsUrl, { reconnectInterval: 3000 });
@@ -51,10 +50,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const liveDrawScene = new LiveDrawScene(engine);
     setupWebsocket(liveDrawScene);
-    
+
     engine.registerScene("live-draw", liveDrawScene);
     engine.setScene("live-draw");
-    
+
     // Start the engine's update and render loop.
     engine.start();
 });
